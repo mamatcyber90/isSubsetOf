7a501d803ba50f77aba25e848d942f514d4b256c
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2017-07-31 11:41:40 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## isSubsetOf
+
+A shell script to check whether all files in folder1 are contained in folder2
---
+++ isSubsetOf.sh
@@ -0,0 +1,1 @@
+diff <(find Award\ Letters/* -exec md5 -q {} \;) <(find ../01\:09\:2017/Award\ Letters/* -exec md5 -q {} \;)
